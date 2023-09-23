d2cc1bfe97d87f3a8094dd63daf508736bf02831
Joe Thorley <joe@poissonconsulting.ca>
2023-09-23 08:32:43 -0700

    documenting

--- R/nmbr-package.R
+++ R/nmbr-package.R
@@ -2,5 +2,14 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @import chk
+#' @import embr
+#' @import extras
+#' @import mcmcr
+#' @import nlist
+#' @import term
+#' @import universals
+#' @importFrom plyr llply
+#' @importFrom stats coef predict update
 ## usethis namespace: end
 NULL
---
+++ tests/testthat/test-is.R
@@ -0,0 +1,3 @@
+test_that("is.nmb_code", {
+  expect_false(is.nmb_code(TRUE))
+})
